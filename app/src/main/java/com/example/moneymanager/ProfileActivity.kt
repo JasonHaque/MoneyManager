064c650487daefec50f5f2d78cd9afabc0cc073a
jason <zahinjason220434@gmail.com>
2019-10-23 16:49:26 +0600

    Added Profile View

--- app/src/main/java/com/example/moneymanager/ProfileActivity.kt
+++ app/src/main/java/com/example/moneymanager/ProfileActivity.kt
@@ -2,11 +2,23 @@ package com.example.moneymanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
+    private var firebaseAuth =FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        bindWidgets()
+        bindListeners()
+    }
+
+    private fun bindWidgets(){
+        Profile_view.setText(firebaseAuth.currentUser?.email.toString())
+    }
+    private fun bindListeners(){
+
     }
 }
