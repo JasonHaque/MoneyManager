f5e9aeaa463c29bdafddce222e3c784aa7e829ac
jason <zahinjason220434@gmail.com>
2019-10-23 22:30:09 +0600

    Added Account details Activity

---
+++ app/src/main/java/com/example/moneymanager/AccountDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moneymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AccountDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_details)
+    }
+}
