13f36bd2171eefe401c4fd9d5ccc1f207a314b33
jason <zahinjason220434@gmail.com>
2019-10-25 15:25:37 +0600

    skip listener added

--- app/src/main/java/com/example/moneymanager/AccountDetailsActivity.kt
+++ app/src/main/java/com/example/moneymanager/AccountDetailsActivity.kt
@@ -1,12 +1,21 @@
 package com.example.moneymanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_account_details.*
 
 class AccountDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_details)
+        bindListeners()
+    }
+
+    fun bindListeners(){
+        skip_attributes.setOnClickListener{
+            startActivity(Intent(this,ProfileActivity::class.java))
+        }
     }
 }
