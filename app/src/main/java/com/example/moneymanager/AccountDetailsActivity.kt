c8b23da47d6b401fe5a1a5c9cccc1c9a318cb851
jason <zahinjason220434@gmail.com>
2019-10-25 15:40:20 +0600

    added class

--- app/src/main/java/com/example/moneymanager/AccountDetailsActivity.kt
+++ app/src/main/java/com/example/moneymanager/AccountDetailsActivity.kt
@@ -3,10 +3,13 @@ package com.example.moneymanager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_account_details.*
 
 class AccountDetailsActivity : AppCompatActivity() {
 
+    private var dref =com.google.firebase.database.FirebaseDatabase.getInstance().reference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_details)
@@ -17,5 +20,7 @@ class AccountDetailsActivity : AppCompatActivity() {
         skip_attributes.setOnClickListener{
             startActivity(Intent(this,ProfileActivity::class.java))
         }
+        dref.child("Users").child("Data").child(FirebaseAuth.getInstance().currentUser.toString()).setValue()
+
     }
 }
