a71abd29e69554f86e99502d2f7d4b4e075862e0
jason <zahinjason220434@gmail.com>
2019-10-26 20:56:22 +0600

    Added firebase db retrieval

--- app/src/main/java/com/example/moneymanager/AccountDetailsActivity.kt
+++ app/src/main/java/com/example/moneymanager/AccountDetailsActivity.kt
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_account_details.*
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class AccountDetailsActivity : AppCompatActivity() {
 
@@ -34,9 +38,31 @@ class AccountDetailsActivity : AppCompatActivity() {
             dref.child("Users").child("Data").child(FirebaseAuth.getInstance().currentUser?.email.toString().split("@")[0])
                 .setValue(Data).addOnSuccessListener {
                     Toast.makeText(this,"Success",Toast.LENGTH_LONG).show()
-                    startActivity(Intent(this,ProfileActivity::class.java))
+                    val ref =dref.child("Users").child("Data")
+                        .child(FirebaseAuth.getInstance().currentUser?.email.toString().split("@")[0])
+                    val listener = object : ValueEventListener {
+                        override fun onCancelled(p0: DatabaseError) {
+                            println("Not Implemented yet")
+                        }
+
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            if (dataSnapshot.exists()){
+                                print(dataSnapshot)
+                                //val datasetter = dataSnapshot.getValue(UserData::class.java)
+                                //println(datasetter?.occupation+"         "+datasetter?.salary)
+                                //occupation_view.text= datasetter?.occupation
+                                //salary_view.text=datasetter?.salary.toString()
+                                //address_view.text=datasetter?.address
+                            }
+
+                        }
+
+                    }
+                    ref.addValueEventListener(listener)
+
                 }
         }
+
 
     }
 }
--- app/src/main/java/com/example/moneymanager/ProfileActivity.kt
+++ app/src/main/java/com/example/moneymanager/ProfileActivity.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
     private var firebaseAuth =FirebaseAuth.getInstance()
+    private var dref = FirebaseDatabase.getInstance().getReference()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -18,6 +23,28 @@ class ProfileActivity : AppCompatActivity() {
 
     private fun bindWidgets(){
         Profile_view.text = firebaseAuth.currentUser?.email.toString()
+        val ref =dref.child("Users").child("Data")
+            .child(FirebaseAuth.getInstance().currentUser?.email.toString().split("@")[0])
+        val listener = object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                println("Not Implemented yet")
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()){
+                    print(dataSnapshot.value)
+                    //val datasetter = dataSnapshot.getValue(UserData::class.java)
+                    //println(datasetter?.occupation+"         "+datasetter?.salary)
+                    //occupation_view.text= datasetter?.occupation
+                    //salary_view.text=datasetter?.salary.toString()
+                    //address_view.text=datasetter?.address
+                }
+
+            }
+
+        }
+        ref.addValueEventListener(listener)
+
     }
     private fun bindListeners(){
         logOut.setOnClickListener{
@@ -28,5 +55,6 @@ class ProfileActivity : AppCompatActivity() {
         set_missing_attributes.setOnClickListener {
             startActivity(Intent(this,AccountDetailsActivity::class.java))
         }
+
     }
 }
