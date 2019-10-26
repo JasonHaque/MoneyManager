e36f2df915501609cc3eb26698b4fa6b687b8c13
JasonHaque <zahinjason220434@gmail.com>
2019-10-26 21:06:27 +0600

    added new activity

---
+++ app/src/main/java/com/example/moneymanager/ManagementActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moneymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ManagementActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_management)
+    }
+}
