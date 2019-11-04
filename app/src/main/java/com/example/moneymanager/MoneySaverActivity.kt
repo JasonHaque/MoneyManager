d3cd2fdb8c6752460874000b14e406d1b4d6492a
JasonHaque <zahinjason220434@gmail.com>
2019-11-04 09:31:56 +0600

    listeners added

--- app/src/main/java/com/example/moneymanager/MoneySaverActivity.kt
+++ app/src/main/java/com/example/moneymanager/MoneySaverActivity.kt
@@ -1,12 +1,33 @@
 package com.example.moneymanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_money_saver.*
 
 class MoneySaverActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_money_saver)
+        bindListeners()
+    }
+    fun bindListeners(){
+        monthly_expenses.setOnClickListener {
+            val intent =Intent(this,MonthlyExpenseActivity::class.java)
+            startActivity(intent)
+        }
+        daily_expenses.setOnClickListener {
+            val intent =Intent(this,DailyExpenseActivity::class.java)
+            startActivity(intent)
+        }
+        set_monthly_expenses.setOnClickListener {
+            val intent =Intent(this,SetMonthlyExpenseActivity::class.java)
+            startActivity(intent)
+        }
+        set_daily_expenses.setOnClickListener {
+            val intent =Intent(this,SetDailyExpenseActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
