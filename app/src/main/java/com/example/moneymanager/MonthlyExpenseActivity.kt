e6ba58b07ec9bd752b03aedecc78d2209b91ff17
JasonHaque <zahinjason220434@gmail.com>
2019-11-04 09:23:07 +0600

    new activity added

---
+++ app/src/main/java/com/example/moneymanager/MonthlyExpenseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moneymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MonthlyExpenseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_monthly_expense)
+    }
+}
