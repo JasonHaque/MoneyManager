74cd93fc4cd20820d25607d76aaed810972d2574
JasonHaque <zahinjason220434@gmail.com>
2019-11-04 09:23:45 +0600

    new activity added

---
+++ app/src/main/java/com/example/moneymanager/SetDailyExpenseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moneymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SetDailyExpenseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_daily_expense)
+    }
+}
