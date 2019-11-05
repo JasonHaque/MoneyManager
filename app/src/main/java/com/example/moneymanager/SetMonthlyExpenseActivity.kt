830d00a4d17d0607fe6ad0955d626cb34fb7ed38
JasonHaque <zahinjason220434@gmail.com>
2019-11-05 12:25:21 +0600

    field checkers added

--- app/src/main/java/com/example/moneymanager/SetMonthlyExpenseActivity.kt
+++ app/src/main/java/com/example/moneymanager/SetMonthlyExpenseActivity.kt
@@ -2,11 +2,28 @@ package com.example.moneymanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_set_monthly_expense.*
 
 class SetMonthlyExpenseActivity : AppCompatActivity() {
-
+    private var dref:DatabaseReference =FirebaseDatabase.getInstance().reference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_set_monthly_expense)
+        bindListeners()
+    }
+    private fun bindListeners() {
+        monthly_amount_setter.setOnClickListener {
+            val monthName =monthly_limit.text.toString()
+            val monthlyLimit = monthly_amount.text.toString()
+            if(monthName.isEmpty() || monthlyLimit.isEmpty()){
+                Toast.makeText(this,"Fill up the fields properly",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val amount =monthlyLimit.toInt()
+
+        }
     }
 }
